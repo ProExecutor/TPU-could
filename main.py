3e0d2bca9fe6bfc58983b43139f9c0dd3ad941cc
ProExecutor <123886692+ProExecutor@users.noreply.github.com>
2024-01-06 16:34:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import tensorflow as tf
-import torch
-import torch_xla.core.xla_model as xm
 
-# TensorFlow TPU initialization
+# Create a TPUClusterResolver
 resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='')
-tf.config.experimental_connect_to_cluster(resolver)
-tf.tpu.experimental.initialize_tpu_system(resolver)
-print("All devices: ", tf.config.list_logical_devices('TPU'))
 
-# PyTorch TPU usage
-dev = xm.xla_device()
-t1 = torch.randn(3,3,device=dev)
-t2 = torch.randn(3,3,device=dev)
-print(t1 + t2)
+# Connect to the TPU cluster
+tf.config.experimental_connect_to_cluster(resolver)
+
+# Initialize the TPU system
+tf.tpu.experimental.initialize_tpu_system(resolver)
+
+# List all TPU devices
+print("All devices: ", tf.config.list_logical_devices('TPU'))
